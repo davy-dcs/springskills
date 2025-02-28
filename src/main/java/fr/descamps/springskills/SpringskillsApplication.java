8337b424ae93638c089141195132af91f7ecf5a0
davy-dcs <davydescamps01@gmail.com>
2025-02-28 16:54:17 +0100

    Structure minimale

---
+++ src/main/java/fr/descamps/springskills/SpringskillsApplication.java
@@ -0,0 +1,13 @@
+package fr.descamps.springskills;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringskillsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringskillsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/descamps/springskills/SpringskillsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.descamps.springskills;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringskillsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
