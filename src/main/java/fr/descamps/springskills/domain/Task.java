2f45f105485cd163ba0a2ea75a6bfcb951fd5765
davy-dcs <davydescamps01@gmail.com>
2025-03-01 15:46:06 +0100

    Entité Task basic créé

---
+++ src/main/java/fr/descamps/springskills/domain/Task.java
@@ -0,0 +1,21 @@
+package fr.descamps.springskills.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+
+import java.sql.Date;
+
+@Builder
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+    private Date date;
+    private TaskStatus status;
+}
---
+++ src/main/java/fr/descamps/springskills/domain/TaskStatus.java
@@ -0,0 +1,7 @@
+package fr.descamps.springskills.domain;
+
+public enum TaskStatus {
+    TO_DO,
+    IN_PROGRESS,
+    DONE
+}
